fa8247e854c9a745bcdccb6e61fd173963b5112d
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2022-08-25 23:15:19 +0300

    Update Features.js

--- src/components/Features.js
+++ src/components/Features.js
@@ -11,38 +11,38 @@ function Features() {
   return (
     <section className="container mt-24 flex flex-col items-center">
       <h2 className="text-[32px] font-bold text-center sm:text-left">
-        Product was Built Specifically for You
+        App was Built Specifically for You
       </h2>
 
       <div className="mt-12 grid sm:grid-cols-2 lg:grid-cols-4 gap-12">
 
       <Feature
         Icon={CursorClickIcon}
-        title="First Click Test"
+        title="SLA Performance Control"
         iconBgColor="#02897A"
-        description="While most people enjoy casino gambling,"
+        description="Automate tracking your SLAs performance!"
       />
       <Feature
         Icon={UsersIcon}
         iconBgColor="#4D8DFF"
-        title="First Click Test"
-        description="While most people enjoy casino gambling,"
+        title="Keep your Team aligned"
+        description="Automate notifications to your Slack!"
       />
       <Feature
         Icon={HeartIcon}
         iconBgColor="#740A76"
-        title="First Click Test"
-        description="While most people enjoy casino gambling,"
+        title="Missed SLA Analysis"
+        description="Deep dives into the right data!"
       />
       <Feature
         Icon={ClockIcon}
         iconBgColor="#F03E3D"
-        title="First Click Test"
-        description="While most people enjoy casino gambling,"
+        title="Hourly Reports"
+        description="The right data for any timeframe!"
       />
       </div>
 
-      <button className="primary-button mt-14">Sign up Now</button>
+      <a href="sladivex@gmail.com"><button>Get in touch now!</button></a>
 
     </section>
   );
